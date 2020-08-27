dbe4160b39bd5c71e86c953ce9defc9fb21a57c2
priyansi <priyanhiman@gmail.com>
2020-08-27 20:57:21 +0530

    pretty please

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
-mkdir -p ~/.streamlit/  
+mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
 email = \"priyanhiman@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\                       
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
